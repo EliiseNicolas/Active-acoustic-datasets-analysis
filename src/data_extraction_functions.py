bba453aac4f1312dd69e88c47a9c7825a4e6b3af
Elise Nicolas <elise.nicolas@protonmail.com>
2025-01-27 15:45:04 +0100

    Extracted seasons and year from dataset, have to plot in order to really understand when the data was recorded (time/season)

--- src/data_extraction_functions.py
+++ src/data_extraction_functions.py
@@ -9,6 +9,8 @@ import netCDF4 as nc
 from netCDF4 import Dataset
 import numpy as np
 import datetime
+from typing import List
+
 
 #%% ------------------------ Extract and convert time/date data
 def get_datetime(dataset:Dataset, convert_datetime=True)->np.ndarray : 
@@ -40,18 +42,65 @@ def get_datetime(dataset:Dataset, convert_datetime=True)->np.ndarray :
         return date_var
     except ValueError:
         print("ValueError: Could not find the TIME variable in the dataset.")
+        return None
 
-def get_season(dataset : Dataset)->str : 
+    
+def get_season(dataset:Dataset) -> List[str]: 
+    """
+    Extracts the season(s) from a dataset based on the datetime of each sample.
+
+    This function assumes that the dataset contains a TIME variable which is 
+    converted into a list of datetime objects. It then sorts the datetime 
+    values and determines in which season each sample was recorded, returning
+    a list of corresponding seasons.
+
+    Parameters:
+    ----------
+    dataset : Dataset
+        The netCDF4 Dataset object containing the TIME variable, which is 
+        used to extract datetime values for each sample.
+
+    Returns:
+    -------
+    List[str]
+        A list of strings representing the season(s) for each sample. 
+        Possible values are "winter", "spring", "summer", and "fall".
+        The list will have the same length as the number of samples in the 
+        dataset, with each element indicating the season of the corresponding 
+        sample.
+    """
+
     # Get datetime in '%Y-%m-%d %H:%M:%S' format
     datetime_list = get_datetime(dataset)
 
     # Sort datetime in increasing order
     sorted_datetime_objects = sorted(datetime_list)
     
-    # Get first and last sample and dertermine the season(s)
-    start_time = sorted_datetime_objects[0]
-    end_time = sorted_datetime_objects[len(sorted_datetime_objects)]
+    # Get season(s) in which data were recorded
+    data_info = dict()
+    for datetime in sorted_datetime_objects : 
+        # Extract month and year from datetime format
+        month = datetime.month
+        year=datetime.year
 
-    print(start_time)
+        # Add year in dict
+        if year not in data_info : 
+            data_info[year] = []
+        
+        # Add season in corresponding year of recording
+        if month in [12, 1, 2]: # Winter 
+            if "winter" not in data_info[year] :
+                data_info[year].append("winter")
+        elif month in [3, 4, 5]: # Spring
+            if "spring" not in data_info[year] : 
+                data_info[year].append("spring")
+        elif month in [6, 7, 8]: # Summer
+            if "summer" not in data_info[year] : 
+                data_info[year].append("summer")
+        elif month in [9, 10, 11]: # fall
+            if "fall" not in data_info[year] :
+                data_info[year].append("fall")
 
-    
+    return data_info
+
+# %%
