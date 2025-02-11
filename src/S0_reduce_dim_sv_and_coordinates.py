bf4004273dbba6c0527dcbbd316f2956bd217da7
Elise Nicolas <elise.nicolas@protonmail.com>
2025-02-11 21:38:58 +0100

    finished script to open .nc in stream. So next step is S1 => Convert data of pickle into 3D matric (max_lat - min_lat, max_long - min_long, depth) with sV values inside so I can visualize data in 3D

---
+++ src/S0_reduce_dim_sv_and_coordinates.py
@@ -0,0 +1,63 @@
+import os
+import xarray as xr
+import pickle
+import sys
+
+# Get args
+src_path = sys.argv[1]
+dest_path = sys.argv[2]
+pickle_file_name = sys.argv[3]
+if len(sys.argv) > 4 :
+    var_name = sys.argv[4]
+else : 
+     var_name = "Sv"
+
+# Create pickle dict
+if not dest_path.endswith("/") :
+     dest_path += "/"
+pickle_file = dest_path + pickle_file_name + ".pkl"
+
+if os.path.exists(pickle_file):
+    os.remove(pickle_file)
+
+# Get every file of folder
+list_files = [f for f in os.listdir(src_path) if f.endswith('.nc')]
+
+for i, file in enumerate(list_files) :
+
+    # Load file in xarray.Dataset
+    file_path = os.path.join(src_path, file)
+    ds=xr.open_dataset(file_path)
+
+    # Create dict containing data of current file
+    data_file = {}
+
+    # Keep coordinates (time, depth, longitude, latitude, channels)
+    if i==0 : # Keep only one copy of depth array cause it is the same for every file
+        data_file["DEPTH"] = ds.coords["DEPTH"].values
+    data_file["TIME"] = ds.coords["TIME"].values
+    data_file["LONGITUDE"] = ds["LONGITUDE"].values
+    data_file["LATITUDE"] = ds["LATITUDE"].values
+    try : 
+        # Not every dataset has the channel data in its attributes, sometimes it is in its coords
+        channel = [ds.attrs["channel"]]
+    except : 
+        channel = ds.coords["CHANNEL"].values
+        channel = [x.decode('utf-8').strip() for x in channel]
+    finally :
+            data_file["CHANNEL"]=channel
+
+
+    # Keep only variable of interest and day
+    data_file["Sv"] = ds[var_name].values #np.ndarray of shape (n_pings, d_depths, n_channels)
+    data_file["DAY"]= ds["day"].values
+
+    # Get title of file
+    title = ds.attrs["title"]
+
+    # Save selected data of current dataset in pickle file
+    with open(pickle_file, 'ab') as p_file:
+        pickle.dump((title, data_file), p_file)
+
+    # Close xarray.Dataset
+    ds.close()  
